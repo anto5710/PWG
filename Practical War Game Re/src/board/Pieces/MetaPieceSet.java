927a8a0da8633ab5bf6b212686e65be22e825328
BuildTools <anto5710@gmail.com>
2020-08-23 14:23:17 +0900

    re

--- Practical War Game Re/src/board/Pieces/MetaPieceSet.java
+++ Practical War Game Re/src/board/Pieces/MetaPieceSet.java
@@ -3,6 +3,7 @@ package board.Pieces;
 public class MetaPieceSet{
 	public final String symbol;
 	public final int team;
+	
 	public MetaPieceSet(String symbol, int team) {
 		this.symbol = symbol;
 		this.team = team;
